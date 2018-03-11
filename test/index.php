2173125f56a6f0a3e3e705e6e42763c340584727
NAAMASLIMA\naama <naama32@hotmail.com>
2018-03-11 11:49:40 -0300

    Test Fix

--- test/index.php
+++ test/index.php
@@ -1,9 +1,9 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '../vendor/autoload.php';
 
 use Notification\Email;
 
-$email = new Email;
-$email->sendEmail("naama32@hotmail.com", "contato@ndevtecnologia.com.br", "ndev", "assunto", "This is the HTML message body <b>in bold!</b>");
+$email = new Email('smtpDebug', 'host', 'username', 'password', 'smtpSecure', 'port', 'setFromEmail', 'setFromName');
+$email->sendEmail('your address email', 'your replay address', 'your replay name', 'subject', 'body');
 
 
